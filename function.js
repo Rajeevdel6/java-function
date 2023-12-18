1ea5ed379a3179e7da186e89be1be48681a6a5fc
Rajeevdel6 <151963374+Rajeevdel6@users.noreply.github.com>
2023-12-18 11:53:42 +0530

    convison of data types

---
+++ function.js
@@ -0,0 +1,14 @@
+//let score = "33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let isLoggedIn =" rajeev"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+//1=>true;0=>false
+//""=>false
+//"rajeev"=> true
+let someNumber =33;
+
+let StringNumber =String(someNumber);
+console.log(String(someNumber));
