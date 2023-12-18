c40c5daabfe4d655b6e30708636cd4b88349eac7
Rajeevdel6 <151963374+Rajeevdel6@users.noreply.github.com>
2023-12-18 12:23:18 +0530

    conversation

--- function.js
+++ function.js
@@ -12,3 +12,25 @@ let someNumber =33;
 
 let StringNumber =String(someNumber);
 console.log(String(someNumber));
+//****************** oprations ************************/
+//let value =3
+//let negValue= -value
+//console.log(negValue);
+//let str1 = "hello"
+//let str2 = "rajeev"
+//let str3 = str1+str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+2+2);
+//console.log(1+2+3+"1");
+//console.log((3+4)*5%3);
+let num1,num2,num3
+num1 =num2=num3=2+2
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
+//study//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
